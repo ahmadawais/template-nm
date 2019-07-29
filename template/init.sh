5c745951e83557b9c4e4925935bfd11da40f6bd7
Ahmad Awais <me@AhmadAwais.com>
2019-07-30 00:47:28 +0500

    📦 NEW: Init script

---
+++ template/init.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+# Create the repo with name and description.
+gh re --new "{{name}}" --description "{{description}}"
+
+# Git stuff.
+git init
+git remote add https://github.com/ahmadawais/{{name}}.git
+
+# Delete yourself, bash script.
+rm -- "$0"
