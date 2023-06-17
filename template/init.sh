6ea60a0e5488422764be60f180db34a4c7240f55
Ahmad Awais ⚡️ <me@AhmadAwais.com>
2023-06-17 17:01:47 +0500

    👌 IMPROVE: No git stuff

--- template/init.sh
+++ template/init.sh
@@ -1,11 +1,4 @@
 #! /usr/bin/env bash
 
-# Create the repo with name and description.
-gh re --new "{{name}}" --description "{{description}}" --homepage "https://twitter.com/MrAhmadAwais"
-
-# Git stuff.
-git init
-git remote add origin https://github.com/ahmadawais/{{name}}.git
-
 # Delete yourself, bash script.
 rm -- "$0"
