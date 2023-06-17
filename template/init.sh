f34b423c45551e6fa2e92fa4b94bb4956eb43fab
Ahmad Awais ⚡️ <me@AhmadAwais.com>
2023-06-17 17:26:04 +0500

    📦 NEW: Git repo

--- template/init.sh
+++ template/init.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
 
+# Set up a git repo.
+git init &>/dev/null
+gh repo create --description "description" --homepage "https://twitter.com/MrAhmadAwais" --public --source=. &>/dev/null
+
+
 # Delete yourself, bash script.
 rm -- "$0"
